948361ed34bd4a14148aa20997d0524ae4300842
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-09-16 14:11:49 +0300

    converted message-sent.html file into react component

---
+++ src/pages/Pricing/components/MessageSent/MessageSent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MessageSent = () => {
+  return (
+    <div class="success-message">
+      <h1>Message Sent</h1>
+      <p>Thank you for your message!</p>
+      <p>We would contact you as soon as possible</p>
+      <button>OK</button>
+    </div>
+  );
+};
+
+export default MessageSent;
